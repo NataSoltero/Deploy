70cda616d944fd82fb1e6b79c0b8f06329dddc38
NataSoltero <39927992+NataSoltero@users.noreply.github.com>
2018-07-09 13:58:29 -0500

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "access_key" {
+	default = "AKIAJCZ7A7J5Y7ZLFTWQ"
+}
+variable "secret_key" {
+	default = "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"
+}
+variable "region" {
+  default = "us-west-2"
+}
