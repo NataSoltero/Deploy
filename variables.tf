07d0e1a96581b4f6b317d8acbd9c2d762e1be638
NataSoltero <39927992+NataSoltero@users.noreply.github.com>
2018-07-09 14:02:58 -0500

    Delete variables.tf

--- variables.tf
+++
@@ -1,9 +0,0 @@
-variable "access_key" {
-	default = "AKIAJCZ7A7J5Y7ZLFTWQ"
-}
-variable "secret_key" {
-	default = "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"
-}
-variable "region" {
-  default = "us-west-2"
-}
