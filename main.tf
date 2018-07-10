3badea5a75f1ef81f94b583e1ff1fff23bd97511
NataSoltero <39927992+NataSoltero@users.noreply.github.com>
2018-07-10 15:46:36 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,42 +6,11 @@ provider "aws" {
 
 data "aws_availability_zones" "all" {}
 
-#VPC
-resource "aws_vpc" "demo" {
-	cidr_block = "192.0.0.0/16"
-}
-
-#Gateway
-resource "aws_internet_gateway" "demo" {
-	vpc_id = "${aws_vpc.demo.id}"
-}
-
-#Internet access
-resource "aws_route" "internet_access" {
-	route_table_id = "${aws_vpc.demo.main_route_table_id}"
-	destination_cidr_block = "0.0.0.0/0"
-	gateway_id = "${aws_internet_gateway.demo.id}"
-}
-
-#Subnet
-resource "aws_subnet" "demo" {
-	vpc_id = "${aws_vpc.demo.id}"
-	cidr_block = "192.0.16.0/20"
-	availability_zone = "us-west-2a"
-}
-
-#Backsubnet
-resource "aws_subnet" "backdemo" {
-	vpc_id = "${aws_vpc.demo.id}"
-	cidr_block = "192.0.32.0/20"
-	availability_zone = "us-west-2b"
-}
-
 #ELB
 resource "aws_elb" "demo" {
 	name = "terraform-asg-demo"
-	subnets = ["${aws_subnet.demo.id}","${aws_subnet.backdemo.id}"]
 	cross_zone_load_balancing = true
+	availability_zones = ["us-west-2a", "us-west-2b", "us-west-2c"]
 	
 	health_check {
 		healthy_threshold = 2
@@ -62,8 +31,8 @@ resource "aws_elb" "demo" {
 #ASG Elasticsearch
 resource "aws_autoscaling_group" "elasticsearch" {
 	launch_configuration = "${aws_launch_configuration.elasticsearch.id}"
-	vpc_zone_identifier = ["${aws_subnet.demo.id}", "${aws_subnet.backdemo.id}"]
 	load_balancers = ["${aws_elb.demo.id}"]
+	availability_zones = ["us-west-2a", "us-west-2b", "us-west-2c"]
 	
 	desired_capacity = 1
 	min_size = 1
@@ -82,7 +51,7 @@ resource "aws_autoscaling_group" "elasticsearch" {
 resource "aws_launch_configuration" "elasticsearch" {
 	image_id = "ami-28e07e50"
 	instance_type = "t2.micro"
-	security_groups = ["${aws_security_group.elasticsearch.id}"]
+	security_groups = ["${aws_security_group.allow_all.id}"]
 	key_name = "demo"
 	
 	name = "Elasticsearch Server"
@@ -93,43 +62,30 @@ resource "aws_launch_configuration" "elasticsearch" {
 }
 
 #SG
-resource "aws_security_group" "elasticsearch" {
-	name = "terraform_elasticsearch"
-	vpc_id = "${aws_vpc.demo.id}"
-		
-#ssh from anywhere
-	ingress {
-		from_port = 22
-		to_port = 22
-		protocol = "tcp"
-		cidr_blocks = ["0.0.0.0/0"]
-	}
+resource "aws_security_group" "allow_all" {
+  name = "allow_all"
+  description = "Allow all inbound traffic"
+  
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
-#http access from the vpc
-	ingress {
-		from_port = 8080
-		to_port = 8080
-		protocol = "tcp"
-		cidr_blocks = ["192.0.0.0/16"]
-	}
-
-	egress {
-		from_port = 0
-		to_port = 0
-		protocol = "-1"
-		cidr_blocks = ["0.0.0.0/0"]
-	}
-	
-	lifecycle {
-		create_before_destroy = true
-	}
+  egress {
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
+    cidr_blocks     = ["0.0.0.0/0"]
+  }
 }
 
 #ASG Logstash
 resource "aws_autoscaling_group" "logstash" {
 	launch_configuration = "${aws_launch_configuration.logstash.id}"
-	vpc_zone_identifier = ["${aws_subnet.demo.id}", "${aws_subnet.backdemo.id}"]
 	load_balancers = ["${aws_elb.demo.id}"]
+	availability_zones = ["us-west-2a", "us-west-2b", "us-west-2c"]
 	
 	desired_capacity = 1
 	min_size = 1
@@ -148,7 +104,7 @@ resource "aws_autoscaling_group" "logstash" {
 resource "aws_launch_configuration" "logstash" {
 	image_id = "ami-28e07e50"
 	instance_type = "t2.micro"
-	security_groups = ["${aws_security_group.logstash.id}"]
+	security_groups = ["${aws_security_group.allow_all.id}"]
 	key_name = "demo"
 	
 	name = "Logstash Server"
@@ -158,44 +114,11 @@ resource "aws_launch_configuration" "logstash" {
 	}
 }
 
-#SG Logstash
-resource "aws_security_group" "logstash" {
-	name = "terraform_logstash"
-	vpc_id = "${aws_vpc.demo.id}"
-		
-#ssh from anywhere
-	ingress {
-		from_port = 22
-		to_port = 22
-		protocol = "tcp"
-		cidr_blocks = ["0.0.0.0/0"]
-	}
-
-#http access from the vpc
-	ingress {
-		from_port = 8080
-		to_port = 8080
-		protocol = "tcp"
-		cidr_blocks = ["192.0.0.0/16"]
-	}
-
-	egress {
-		from_port = 0
-		to_port = 0
-		protocol = "-1"
-		cidr_blocks = ["0.0.0.0/0"]
-	}
-	
-	lifecycle {
-		create_before_destroy = true
-	}
-}
-
 #ASG Kibana
 resource "aws_autoscaling_group" "kibana" {
 	launch_configuration = "${aws_launch_configuration.kibana.id}"
-	vpc_zone_identifier = ["${aws_subnet.demo.id}", "${aws_subnet.backdemo.id}"]
 	load_balancers = ["${aws_elb.demo.id}"]
+	availability_zones = ["us-west-2a", "us-west-2b", "us-west-2c"]
 	
 	desired_capacity = 1
 	min_size = 1
@@ -214,7 +137,7 @@ resource "aws_autoscaling_group" "kibana" {
 resource "aws_launch_configuration" "kibana" {
 	image_id = "ami-28e07e50"
 	instance_type = "t2.micro"
-	security_groups = ["${aws_security_group.kibana.id}"]
+	security_groups = ["${aws_security_group.allow_all.id}"]
 	key_name = "demo"
 	
 	name = "Kibana Server"
@@ -224,44 +147,11 @@ resource "aws_launch_configuration" "kibana" {
 	}
 }
 
-#SG Kibana
-resource "aws_security_group" "kibana" {
-	name = "terraform_kibana"
-	vpc_id = "${aws_vpc.demo.id}"
-		
-#ssh from anywhere
-	ingress {
-		from_port = 22
-		to_port = 22
-		protocol = "tcp"
-		cidr_blocks = ["0.0.0.0/0"]
-	}
-
-#http access from the vpc
-	ingress {
-		from_port = 8080
-		to_port = 8080
-		protocol = "tcp"
-		cidr_blocks = ["192.0.0.0/16"]
-	}
-
-	egress {
-		from_port = 0
-		to_port = 0
-		protocol = "-1"
-		cidr_blocks = ["0.0.0.0/0"]
-	}
-	
-	lifecycle {
-		create_before_destroy = true
-	}
-}
-
 #ASG Web
 resource "aws_autoscaling_group" "web" {
 	launch_configuration = "${aws_launch_configuration.web.id}"
-	vpc_zone_identifier = ["${aws_subnet.demo.id}", "${aws_subnet.backdemo.id}"]
 	load_balancers = ["${aws_elb.demo.id}"]
+	availability_zones = ["us-west-2a", "us-west-2b", "us-west-2c"]
 	
 	desired_capacity = 1
 	min_size = 1
@@ -280,43 +170,10 @@ resource "aws_autoscaling_group" "web" {
 resource "aws_launch_configuration" "web" {
 	image_id = "ami-28e07e50"
 	instance_type = "t2.micro"
-	security_groups = ["${aws_security_group.web.id}"]
+	security_groups = ["${aws_security_group.allow_all.id}"]
 	key_name = "demo"
-	
-	name = "Web Server"
-	
-	lifecycle {
-		create_before_destroy = true
-	}
-}
-
-#SG Web
-resource "aws_security_group" "web" {
-	name = "terraform_web"
-	vpc_id = "${aws_vpc.demo.id}"
 		
-#ssh from anywhere
-	ingress {
-		from_port = 22
-		to_port = 22
-		protocol = "tcp"
-		cidr_blocks = ["0.0.0.0/0"]
-	}
-
-#http access from the vpc
-	ingress {
-		from_port = 8080
-		to_port = 8080
-		protocol = "tcp"
-		cidr_blocks = ["192.0.0.0/16"]
-	}
-
-	egress {
-		from_port = 0
-		to_port = 0
-		protocol = "-1"
-		cidr_blocks = ["0.0.0.0/0"]
-	}
+	name = "Web Server"
 	
 	lifecycle {
 		create_before_destroy = true
